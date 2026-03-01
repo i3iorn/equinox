5925d749a0c8bbc1c1707824003785f0ab977fce
Björn Schrammel <github@schrammel.dev>
2026-03-01 23:25:13 +0100

    Add reusable GUI widgets for single-responsibility design

---
+++ src/equinox/gui/widgets/__init__.py
@@ -0,0 +1,14 @@
+"""Reusable GUI widgets extracted for single-responsibility."""
+
+from equinox.gui.widgets.url_line_edit import UrlLineEdit
+from equinox.gui.widgets.key_value_table import KeyValueTable
+from equinox.gui.widgets.checkable_key_value_table import CheckableKeyValueTable
+from equinox.gui.widgets.json_body_editor import JsonBodyEditor
+
+__all__ = [
+    "UrlLineEdit",
+    "KeyValueTable",
+    "CheckableKeyValueTable",
+    "JsonBodyEditor",
+]
+
