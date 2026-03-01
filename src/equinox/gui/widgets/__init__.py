8ef1f2b1ac3f59439e723b93e3d28638e1781060
Björn Schrammel <github@schrammel.dev>
2026-03-01 23:33:02 +0100

    Add secret row widget to the public API

--- src/equinox/gui/widgets/__init__.py
+++ src/equinox/gui/widgets/__init__.py
@@ -4,11 +4,13 @@ from equinox.gui.widgets.url_line_edit import UrlLineEdit
 from equinox.gui.widgets.key_value_table import KeyValueTable
 from equinox.gui.widgets.checkable_key_value_table import CheckableKeyValueTable
 from equinox.gui.widgets.json_body_editor import JsonBodyEditor
+from equinox.gui.widgets.secret_row import make_secret_row
 
 __all__ = [
     "UrlLineEdit",
     "KeyValueTable",
     "CheckableKeyValueTable",
     "JsonBodyEditor",
+    "make_secret_row",
 ]
 
