8e1f0ca416453b03495765f83a19f1167d6291b5
Björn Schrammel <github@schrammel.dev>
2026-03-10 22:41:22 +0100

    Expose RequestTimeoutError in package exports and update __all__ list

--- src/equinox/core/__init__.py
+++ src/equinox/core/__init__.py
@@ -2,6 +2,9 @@
 
 from equinox.core.client import HTTPClient
 from equinox.core.request import Request, Response
-from equinox.core.exceptions import EquinoxError, RequestError, AuthError
+from equinox.core.exceptions import EquinoxError, RequestError, AuthError, RequestTimeoutError
 
-__all__ = ["HTTPClient", "Request", "Response", "EquinoxError", "RequestError", "AuthError"]
+__all__ = [
+    "HTTPClient", "Request", "Response",
+    "EquinoxError", "RequestError", "AuthError", "RequestTimeoutError",
+]
