a60469c70e7f597a7a5c3467309a6fa38ccaf3f6
Björn Schrammel <github@schrammel.dev>
2026-03-19 15:32:53 +0100

    Fix timezone handling in utc_now function for improved accuracy

--- src/equinox/core/time.py
+++ src/equinox/core/time.py
@@ -6,5 +6,5 @@ def utc_now(ts: Optional[datetime] = None) -> datetime:
     """Return the current UTC time as a naive datetime (no tzinfo)."""
     tzinfo = None
     if ts:
-        tzinfo = ts.astimezone(timezone.utc)
+        tzinfo = ts.astimezone(timezone.utc).tzinfo
     return datetime.now(timezone.utc).replace(tzinfo=tzinfo)
