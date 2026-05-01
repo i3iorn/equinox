f5fe8874a1336c7253ccdf738761ceeb8bfc0052
Björn Schrammel <github@schrammel.dev>
2026-05-01 18:26:27 +0200

    Refactor __init__.py: add crypto module to exports for enhanced security features

--- src/equinox/core/__init__.py
+++ src/equinox/core/__init__.py
@@ -3,8 +3,10 @@
 from equinox.core.client import HTTPClient
 from equinox.core.request import Request, Response
 from equinox.core.exceptions import EquinoxError, RequestError, AuthError, RequestTimeoutError
+from equinox.security import crypto
 
 __all__ = [
     "HTTPClient", "Request", "Response",
     "EquinoxError", "RequestError", "AuthError", "RequestTimeoutError",
+    "crypto",
 ]
