0d27c6258de9ad89184e29ff85b2546a712419c6
Björn Schrammel <github@schrammel.dev>
2026-05-27 14:37:53 +0200

    feat: add TabToolbar and TextEditorProxy to __init__.py for enhanced widget availability

--- src/equinox/gui/widgets/__init__.py
+++ src/equinox/gui/widgets/__init__.py
@@ -1,12 +1,13 @@
 """Reusable GUI widgets extracted for single-responsibility."""
-
 from equinox.gui.widgets.checkable_key_value_table import CheckableKeyValueTable
 from equinox.gui.widgets.copyable_message_box import CopyableMessageBox
-from equinox.gui.widgets.drag_drop_tree import DragDropTree  # noqa: F401
+from equinox.gui.widgets.drag_drop_tree import DragDropTree
 from equinox.gui.widgets.json_body_editor import JsonBodyEditor
 from equinox.gui.widgets.key_value_table import KeyValueTable
 from equinox.gui.widgets.path_params_table import PathParamsTable
 from equinox.gui.widgets.secret_row import make_secret_row
+from equinox.gui.widgets.tab_toolbar import TabToolbar
+from equinox.gui.widgets.text_editor_proxy import TextEditorProxy
 from equinox.gui.widgets.url_line_edit import UrlLineEdit
 
 __all__ = [
@@ -18,4 +19,6 @@ __all__ = [
     "PathParamsTable",
     "DragDropTree",
     "CopyableMessageBox",
+    "TabToolbar",
+    "TextEditorProxy",
 ]
