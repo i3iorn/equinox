5142483cdea30c6993a1014fea03a33222321c3c
Björn Schrammel <github@schrammel.dev>
2026-09-14 17:30:58 +0200

    fix: bump version to 0.5.2

--- src/equinox/__init__.py
+++ src/equinox/__init__.py
@@ -2,7 +2,7 @@
 Equinox - A local-first API testing tool
 """
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "Björn Schrammel"
 
 from equinox.auth import APIKeyAuth, AuthStrategy, BasicAuth, BearerAuth
